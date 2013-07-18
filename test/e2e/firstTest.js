f8d913bec54e6ae7a3ba26db65e1a8d92257e49d
Jefferson Oliveira <jefferson.oliveira@ideais.com.br>
2013-07-18 14:46:33 -0300

    teste End-to-End verifica na Theads CAT, CKT e MAN

--- test/e2e/firstTest.js
+++ test/e2e/firstTest.js
@@ -13,5 +13,13 @@ describe('/index.html', function() {
     it ('Deve ter 9 THs no cabecalho', function() {
       expect(repeater('table thead th').count()).toBe(9)
     })
+    
+    it('Deve haver dentro das Theads em th os conteudos CAT, CKT e MAN', function(){
+			expect(element('table thead tr th:nth-child(2)').text()).toEqual('CAT')
+			expect(element('table thead tr th:nth-child(3)').text()).toEqual('CKT')
+			expect(element('table thead tr th:nth-child(6)').text()).toEqual('MAN')
+	})
+	
+
 
 })
