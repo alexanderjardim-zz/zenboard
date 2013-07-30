81aed3ba38bb22fbf28cee2db6f17d045d59fc95
Larissa <larissa.teixeira@ideais.com.br>
2013-07-30 18:12:26 -0300

    Teste unitario

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,4 +1,4 @@
-//use strict';       
+'use strict';       
 
 describe("zenboard controllers", function() {
 	
@@ -7,10 +7,33 @@ describe("zenboard controllers", function() {
 		it("Valor da primeira celula da tabela deve ser 1", function() {
 			var scope = [],
 			ctrl = new ZDViewCtrl(scope); 
-			expect(scope.tickets[0].count).toBe("1");
+			expect(scope.tickets[0].count).toBe("1"); 
 		});
+	});
+}); 
+/*'use strict';   
 
+describe("zenboard controllers", function() {
+ 
+ describe("ZDViewCtrl", function() {
+    var scope, ctrl, $httpBackend;
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('views/29639836.json').
+          respond([{count: "21"}]);
+ 
+      scope = $rootScope.$new();
+      ctrl = $controller(ZDViewCtrl, {$scope: scope});
+    }));
+
+
+	it('Valor da primeira celula da tabela deve ser 1', function() {
+	      expect(scope.tickets).toBeUndefined();
+	      $httpBackend.flush();
+
+	      expect(scope.tickets[0].count).toBe("1"); 
 	});
 
-});
- 
+
+  });
+});*/
